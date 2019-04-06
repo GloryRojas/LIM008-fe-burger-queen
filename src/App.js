3c13096a27e4fab9bf7d528eea5323e35f7353e2
GloryRojas <grojasm94@gmail.com>
2019-04-05 19:51:39 -0500

    Agregando estructura con App

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
 import React, { Component } from 'react';
 import './App.css';
-
-
+import Header from './header';
+import { Menu, Desayuno } from './menu';
 
 class App extends Component {
-  
   render() {
-    return <h1>'Hola' </h1>
-}};
+    return ( 
+      <div>
+        <Header />
+        <div>
+          <h3>MENÚ</h3>
+          <button>Desayuno</button>
+          <button>Resto del Día</button>
+          <Menu />
+          <Desayuno />
+        </div>
+        <div>
+          <h3>AQUI VAN LOS PEDIDOS</h3>
+        </div>
+      </div> 
+    )};
+};
 
 export default App;
--- src/menu.js
+++ src/menu.js
@@ -2,10 +2,9 @@ import React, { Component } from 'react';
 import firebase from './firestore.js';
 import './App.css';
 
-
 const db = firebase.firestore();
 
-class Menu extends Component {
+export class Desayuno extends Component {
   constructor(props) {
     super(props);
     this.state = {
@@ -41,43 +40,84 @@ class Menu extends Component {
 
   render() {
     const { isLoaded, items } = this.state;
-
     if(!isLoaded) {
       return <div>Loading...</div>
     } else{
         return (
             <div className='productos'>
                 <div className='prod-desayuno'>
-                    <h3>DESAYUNO</h3>
+                    <h4>DESAYUNO</h4>
                     { 
                         items.map(item => {
-                            if (item.categoria === 'Desayuno') { 
+                          if (item.categoria === 'Desayuno') { 
                         return ( 
-                            <div key={item.id}>
-                                <p>{item.nombre}: {item.precio}.00</p>
-                            </div>               
-                            )}
-                        })
-                    }
-                </div>
-                <div className='prod-rest'>
-                    <h3>RESTO DEL DÍA</h3>
-                    { 
-                        items.map(item => {
-                            if (item.categoria === 'allDay') { 
-                        return ( 
-                            <div key={item.id}>
-                                <p>{item.nombre}: {item.precio}.00</p>
-                            </div>               
+                            <p key={item.id}>{item.nombre}: {item.precio}</p>
                             )}
                         })
                     }
                 </div>
             </div>
-
       )
     }
   }
 }
 
-export default Menu;
+export class Menu extends Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+        items: [],
+        isLoaded: false
+      }
+    }
+  
+  /*   componentDidMount() {
+      fetch('https://raw.githubusercontent.com/GloryRojas/LIM008-fe-burger-queen/developer/src/menu/menu.json')
+        .then(res => res.json())
+        .then(json => {
+          this.setState({
+            isLoaded: true,
+            items: json
+          })
+        });
+    } */
+  
+    componentDidMount() {
+      db.collection('menu').get()
+      .then(json => {
+        let doc = []
+        json.forEach(docu => {
+          doc.push(docu.data());
+          this.setState({
+            isLoaded: true,
+            items: doc
+          })
+        })
+        });
+    }
+  
+    render() {
+      const { isLoaded, items } = this.state;
+  
+      if(!isLoaded) {
+        return <div>Loading...</div>
+      } else{
+            return (
+                <div className='productos'>
+                    <div className='prod-rest'>
+                        <h4>RESTO DEL DÍA</h4>
+                        { 
+                            items.map(item => {
+                                if (item.categoria === 'allDay') { 
+                            return ( 
+                                <p key={item.id}>{item.nombre}: {item.precio}.00</p>               
+                                )}
+                            })
+                        }
+                    </div>
+                </div>
+  
+        )
+      }
+    }
+  }
