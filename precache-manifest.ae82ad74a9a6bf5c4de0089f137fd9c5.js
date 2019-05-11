a7a74d0873e95eeba01c0843aadbc2b72213a3dd
GloryRojas <grojasm94@gmail.com>
2019-05-11 15:20:36 -0500

    Updates

--- precache-manifest.ae82ad74a9a6bf5c4de0089f137fd9c5.js
+++ precache-manifest.ae82ad74a9a6bf5c4de0089f137fd9c5.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/LIM008-fe-burger-queen/static/js/runtime~main.10776b7b.js"
   },
   {
-    "revision": "c69b7655a20dba3e3420",
-    "url": "/LIM008-fe-burger-queen/static/js/main.ca810bab.chunk.js"
+    "revision": "24f1b896c9b452a26806",
+    "url": "/LIM008-fe-burger-queen/static/js/main.caf5d748.chunk.js"
   },
   {
     "revision": "b02398e36676cf043bd9",
     "url": "/LIM008-fe-burger-queen/static/js/2.d60d4488.chunk.js"
   },
   {
-    "revision": "c69b7655a20dba3e3420",
+    "revision": "24f1b896c9b452a26806",
     "url": "/LIM008-fe-burger-queen/static/css/main.b4c6fabe.chunk.css"
   },
   {
-    "revision": "a652cdd0a8cef7b2c9be84ac6e11e05b",
+    "revision": "1f5834925d0f6a84180f94109e970fb6",
     "url": "/LIM008-fe-burger-queen/index.html"
   }
 ];
