c339dc080004e7c5fa1737a04304e961eabf1cc2
gloryrojas <glory.rojas>
2020-06-13 22:19:00 -0500

    Updates

---
+++ precache-manifest.92b5fb64e264f90783a56693e3c330d1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "080bfdaad8430eff2022",
+    "url": "/LIM008-fe-burger-queen/static/css/main.968b9ef1.chunk.css"
+  },
+  {
+    "revision": "080bfdaad8430eff2022",
+    "url": "/LIM008-fe-burger-queen/static/js/main.7e98ddbc.chunk.js"
+  },
+  {
+    "revision": "3a4f816d571dde5ac940",
+    "url": "/LIM008-fe-burger-queen/static/js/runtime~main.10776b7b.js"
+  },
+  {
+    "revision": "859b5bc1a51862153c82",
+    "url": "/LIM008-fe-burger-queen/static/js/2.7cace621.chunk.js"
+  },
+  {
+    "revision": "f63a319119fd8ce036bcceda4b71e243",
+    "url": "/LIM008-fe-burger-queen/index.html"
+  }
+];
